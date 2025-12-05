e28a05febb6ec5f625ddb9a45e8182b7463f5e24
Furokawa <116357034+Cptquacks@users.noreply.github.com>
2025-12-05 05:43:33 -0500

    Add files via upload

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,6 +1,6 @@
 import json
 
-def read_csv_lines(path: str, parse_symbol: str = ';') -> list[list[str]]:
+def read_csv_lines(path: str, parse_symbol: str = '|') -> list[list[str]]:
     new_csv_lines : list[list[str]] = []
 
     with open(file = f'{path}', mode = 'r', encoding='UTF-8') as readen_file: 
@@ -15,12 +15,16 @@ def read_csv_lines(path: str, parse_symbol: str = ';') -> list[list[str]]:
 
 def convert_csv_lines(path: str, primary_key: str = '') -> dict:
     print(f'Diving into {path}')
+    if primary_key.__len__() > 0 : primary_key += '-'
+
     new_json_lines : dict = {}
     get_csv_lines : list[list[str]] = read_csv_lines(path)
     csv_lines_heading : list[str] = get_csv_lines[0]
     
-    x : int = 0; y : int = 0
+
+    x : int = 0;y : int = 0
     key_index : int = 0
+
 
     for line in get_csv_lines:
         if (key_index != 0) : 
@@ -28,8 +32,10 @@ def convert_csv_lines(path: str, primary_key: str = '') -> dict:
 
         key_index += 1
 
+
     print(f'generated {key_index-2} id\'s \n')
     key_index = 0
+
 
     for line in get_csv_lines:
 
@@ -43,7 +49,7 @@ def convert_csv_lines(path: str, primary_key: str = '') -> dict:
 
             x+=1
 
-        key_index += 1; x = 0
+        key_index += 1;x = 0
 
 
 
@@ -51,4 +57,9 @@ def convert_csv_lines(path: str, primary_key: str = '') -> dict:
 
 def write_to_json(output: str, data: dict) -> None:
     with open(file=f'{output}', mode='w', encoding='UTF-8') as write_to:
-        json.dump(data, write_to)
+        json.dump(data, write_to, indent=4, separators=(',', ':'),)
+
+def write_to_jsonl(output: str, data: dict) -> None:
+    new_json_list : list[dict] = [data]
+    with open(file=f'{output}', mode='w', encoding='UTF-8') as write_to:
+        json.dump(new_json_list, write_to, indent=4, separators=(',', ':'),)
